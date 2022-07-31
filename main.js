5e4266753d6c8de431ef45f148ff45c2b58264ef
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-07-31 02:46:06 +0100

    ...

---
+++ main.js
@@ -0,0 +1,17 @@
+const operator = prompt("Enter operator (either +, -, * or /): ");
+const num1 =  parseFloat( prompt("Enter first number: "));
+const num2 = parseFloat( prompt("Enter second number: "));
+alert("Check the result in the console");
+let result;
+
+
+if (operator == "+") {
+  result = eval(num1 + num2);
+} else if (operator == "-") {
+  result = num1 - num2;
+} else if (operator == "*") {
+  result = num1 * num2;
+} else {
+  result = num1 / num2;
+}
+console.log(`${num1} ${operator} ${num2} = ${result}`);
